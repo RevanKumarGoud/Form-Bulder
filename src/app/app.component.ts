b576b4a41825a87f529a3a80c664e5e3ce6c2f93
[RevankumarGoud] <[revankumargoudkurmindla@gmail.com]>
2022-07-28 03:23:11 +0530

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Form-Bulder';
+  constructor(private fb : FormBuilder){}
+  registationForm = this.fb.group({
+    UserName: ['', [Validators.required,Validators.minLength(6)]],
+    Password:[''],
+    ConfirmPassword : [''],
+   address: this.fb.group({
+    city : [''],
+    State : [''],
+    PostalCode : ['']
+   })
+  });
+
+  loadApiData(){
+    this.registationForm.setValue({
+      UserName: 'Vikram',
+      Password: 'testqw',
+      ConfirmPassword: 'testqw',
+      address: {
+        city : 'city',
+        State : 'State',
+        PostalCode : '123456'
+      }
+    })
+  }
 }
